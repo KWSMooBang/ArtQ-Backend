140c3afd4783b1eceb0a1826696424513fdd0e2e
KWSMooBang <40kimwoosung@gmail.com>
2025-09-14 21:24:06 +0900

    Add TokenVerifyView in urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, 
-    TokenRefreshView
+    TokenRefreshView,
+    TokenVerifyView,
 )
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -34,8 +35,10 @@ urlpatterns = [
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     
-    # JWT 
+    # Auth (JWT)
     path('v1/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('v1/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/auth/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
     path('v1/auth/', include('apps.accounts.urls')),
 ]
