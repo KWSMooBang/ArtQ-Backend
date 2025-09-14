5598890f2e09ce030f2c8d782e66d6daf03c2386
KWSMooBang <40kimwoosung@gmail.com>
2025-09-14 20:58:52 +0900

    Implement SignupSerializer

---
+++ src/apps/accounts/serializers.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'nickname', 'profile_image_url', 'prefs']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
