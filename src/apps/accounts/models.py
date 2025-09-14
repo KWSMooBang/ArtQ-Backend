ccae1dc0d53f2403d84656fdd883be3fadde8f76
KWSMooBang <40kimwoosung@gmail.com>
2025-09-15 00:15:31 +0900

    Add validators

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -1,17 +1,28 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
 import uuid
 from django.utils import timezone
 from datetime import date
 
+phone_validator = RegexValidator(
+    regex=r"^\+?[0-9]{9,15}$",
+    message="Phone number should be 9~15 digits long."
+)
+nickname_validator = RegexValidator(
+    # 한글/영문/숫자/언더스코어, 2~20자
+    regex=r"^[\w가-힣]{2,20}$",
+    message="Nickname must be 2~20 characters long and can include letters, numbers, and underscores."
+)
+
 class User(AbstractUser):
     # user UUID as basic Public Key
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=20, unique=True, null=True, blank=True, validators=[])
+    phone = models.CharField(max_length=20, unique=True, null=True, blank=True, validators=[phone_validator])
     full_name = models.CharField(max_length=150, null=True, blank=True)
     birth_date = models.DateField()
-    nickname = models.CharField(max_length=20, unique=True, validators=[])
+    nickname = models.CharField(max_length=20, unique=True, validators=[nickname_validator])
     
     email_verified = models.BooleanField(default=False)
     phone_verified = models.BooleanField(default=False)
