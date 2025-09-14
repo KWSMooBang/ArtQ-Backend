352e4e437a56a0aacc372666eaca8352e3902c5f
KWSMooBang <40kimwoosung@gmail.com>
2025-09-14 20:52:35 +0900

    implement User model

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
-# Create your models here.
+class User(AbstractUser):
+    # user UUID as basic Public Key
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nickname = models.CharField(max_length=30, unique=True)
+    
+    profile_image_url = models.URLField(blank=True, null=True)
+    prefs = models.JSONField(default=dict, blank=True)
+    
+    def __str__(self):
+        return self.nickname or self.username
