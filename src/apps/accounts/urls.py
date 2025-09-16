9c365fa5f405a26072757c926d8297eac2ae464d
KWSMooBang <40kimwoosung@gmail.com>
2025-09-16 23:11:00 +0900

    Update urls

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -4,5 +4,6 @@ from .views import RegistrationView, LoginView
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LoginView.as_view(), name='logout'),  # Placeholder for logout view
 ]
