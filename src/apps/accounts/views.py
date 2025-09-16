d5924ab209dfe4faa09073d743ecd29da528fdce
KWSMooBang <40kimwoosung@gmail.com>
2025-09-16 22:22:10 +0900

    Rename SignupView as RegistrationView and update

--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -2,32 +2,51 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from drf_spectacular.utils import extend_schema, OpenApiResponse, OpenApiExample
-from .serializers import SignupSerializer
+from .serializers import RegistrationSerializer
 
-class SignupView(APIView):
+class RegistrationView(APIView):
     @extend_schema(
         tags=["auth"],
-        operation_id="signup",
-        description="Create a new user account.",
-        request=SignupSerializer,
+        operation_id="registration",
+        description=(
+            "Create a new user account.",
+            "first_name, last_name, username, password, nickname are required fields.",
+            "email or phone number are required for verification.",
+            "birth_date is optional.",
+        ),
+        request=RegistrationSerializer,
         responses={
-            201: OpenApiResponse(description="User created successfully."),
+            201: OpenApiResponse(description="Register new user successfully."),
             400: OpenApiResponse(description="Invalid input data."),
         },
         examples=[
             OpenApiExample(
-                "Request example",
+                "Request example (email)",
                 value={
-                    'username': 'honggildong',
-                    'email': 'honggildong@example.com',
-                    'password': 'password1234',
+                    'first_name': 'Gildong',
+                    'last_name': 'Hong',
+                    'username': 'loginID',
+                    'password': 'loginPW',
                     'nickname': '홍길동',
+                    'email': 'honggildong@example.com',
+                    'birth_date': '1990-01-01',
+                }
+            ),
+            OpenApiExample(
+                "Request example (phone)",
+                value={
+                    'first_name': 'Gildong',
+                    'last_name': 'Hong',
+                    'username': 'loginID',
+                    'password': 'loginPW',
+                    'nickname': '홍길동',
+                    'phone': '01012345678',
                 }
             )
         ]
     )
     def post(self, request):
-        serializer = SignupSerializer(data=request.data)
+        serializer = RegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"ok": True}, status=status.HTTP_201_CREATED)
