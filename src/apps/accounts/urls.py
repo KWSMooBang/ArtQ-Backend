65ba34aa1be3c915a92fb3a9c2e10ff0387bc02f
KWSMooBang <40kimwoosung@gmail.com>
2025-09-21 20:42:22 +0900

    Add profile view in urls

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegistrationView, LoginView, LogoutView
+from .views import RegistrationView, LoginView, LogoutView, ProfileView
 
 
 urlpatterns = [
     path('registration/', RegistrationView.as_view(), name='registration'),
     path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),  # Placeholder for logout view
+    path('logout/', LogoutView.as_view(), name='logout'),  
+    path('profile/', ProfileView.as_view(), name='profile'),  
 ]
